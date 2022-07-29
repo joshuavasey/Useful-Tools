587a341c4d094d80829d9cfef35bd548ebb1defb
Josh Vasey <josh.vasey@me.com>
2022-07-29 16:49:54 +0100

    Uploaded 2x PS scripts
    Useful for RDS printer deployment

---
+++ Print Script.ps1
@@ -0,0 +1,34 @@
+﻿# Get list of connected printers
+
+$printers = Get-CimInstance -Class Win32_Printer -Property Name | select Name, Default
+
+$printer1 = "*FollowMe Sharp*"
+
+# Set booleans as to if certain printers are connected.
+
+foreach($printer in $printers) {
+
+    if ($printer -like $printer1) {
+
+        if ($printer -like "*TF-DC.teamfostering.co.uk*") {
+
+            (New-Object -ComObject WScript.Network).SetDefaultPrinter('\\TF-DC.teamfostering.com\TF FollowMe Sharp')
+
+        } elseif ($printer -like "*TF-DC*") {
+
+            (New-Object -ComObject WScript.Network).SetDefaultPrinter('\\TF-DC\TF FollowMe Sharp')
+
+        }
+
+    } else {
+
+        Add-Printer -ConnectionName "\\TF-DC.teamfostering.com\TF FollowMe Sharp"
+
+        (New-Object -ComObject WScript.Network).SetDefaultPrinter('\\TF-DC.teamfostering.com\TF FollowMe Sharp')
+    }
+
+}
+
+# Check new default printer
+
+$defaultPrinter = Get-WmiObject -Query " SELECT * FROM Win32_Printer WHERE Default=$true" | Select Name
---
+++ Scheduled Task.ps1
@@ -0,0 +1,24 @@
+﻿# Check if task exists. Change Variable to whatever is required
+
+$taskName = "Default Printer"
+$taskExists = Get-ScheduledTask | Where-Object {$_.TaskName -Like $taskName}
+
+if($taskExist) {
+
+    # Output logging
+
+    echo "Task Exists" | Add-Content -Path "$env:APPDATA\PrintTask.txt"
+
+} else {
+
+    # Create Scheduled Task to run immediately and every 5 minutes after, can change action and frequency here
+
+    $ac = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-ExecutionPolicy Bypass -WindowStyle Hidden -File \\tf-dc\LicencingTokens\TFPrint.ps1"
+    $tr = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 5)
+    $tsk = Register-ScheduledTask -TaskName $taskName -Trigger $tr -Action $ac
+
+    #Run task after creating it.
+
+    Start-ScheduledTask -TaskName $taskName
+
+}
